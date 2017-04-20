a87a08b93aa5f047b9d99b6450695a98cb124b96
Sean Nam <namsangwoo1@gmail.com>
2017-04-19 18:25:26 -0700

    Initial commit

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
